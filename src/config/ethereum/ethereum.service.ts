ac67eda804f84896c5469323e18e15dddd2ddc8f
ternyavsky <ilya.ternyavsky@yandex.ru>
2025-04-23 10:38:28 +0300

    add mock eth

--- src/config/ethereum/ethereum.service.ts
+++ src/config/ethereum/ethereum.service.ts
@@ -1,7 +1,10 @@
 import { Injectable } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
 import Web3 from 'web3';
-import { mockContractResponse } from './mocks/ethereum.mocks';
+import {
+  mockContractResponse,
+  mockTransactionReceipt,
+} from './mocks/ethereum.mocks';
 
 @Injectable()
 export class EthereumService {
@@ -23,38 +26,44 @@ export class EthereumService {
   }
 
   generateAddress(): { address: string; privateKey: string } {
-    const account = this.web3.eth.accounts.create();
+    // const account = this.web3.eth.accounts.create();
+    // return {
+    //   address: account.address,
+    //   privateKey: account.privateKey,
+    // };
     return {
-      address: account.address,
-      privateKey: account.privateKey,
+      address: '0x1234567890123456789012345678901234567890',
+      privateKey:
+        '0x1234567890123456789012345678901234567890123456789012345678901234',
     };
   }
 
   validateAddress(address: string): boolean {
-    try {
-      this.web3.utils.toChecksumAddress(address);
-      return true;
-    } catch {
-      return false;
-    }
+    // try {
+    //   this.web3.utils.toChecksumAddress(address);
+    //   return true;
+    // } catch {
+    //   return false;
+    // }
+    return true;
   }
 
   async getTokenBalance(address: string): Promise<number> {
     try {
-      //   const contract = new this.web3.eth.Contract(
-      //     [
-      //       {
-      //         constant: true,
-      //         inputs: [{ name: '_owner', type: 'address' }],
-      //         name: 'balanceOf',
-      //         outputs: [{ name: 'balance', type: 'uint256' }],
-      //         type: 'function',
-      //         stateMutability: 'view',
-      //         payable: false,
-      //       },
-      //     ] as const,
-      // this.TOKEN_ADDRESS,
-      //   );
+      // const contract = new this.web3.eth.Contract(
+      //   [
+      //     {
+      //       constant: true,
+      //       inputs: [{ name: '_owner', type: 'address' }],
+      //       name: 'balanceOf',
+      //       outputs: [{ name: 'balance', type: 'uint256' }],
+      //       type: 'function',
+      //       stateMutability: 'view',
+      //       payable: false,
+      //     },
+      //   ] as const,
+      //   this.TOKEN_ADDRESS,
+      // );
       const contract = mockContractResponse;
       const balance = await contract.methods.balanceOf(address).call();
       return Number(balance) / 10 ** 18;
@@ -63,49 +72,46 @@ export class EthereumService {
       throw error;
     }
   }
+
   async transferFromFiat(address: string, amount: number): Promise<boolean> {
     try {
-      const contract = new this.web3.eth.Contract(
-        [
-          {
-            constant: false,
-            inputs: [
-              { name: 'account', type: 'address' },
-              { name: 'amount', type: 'uint256' },
-            ],
-            name: 'transferFromFiat',
-            outputs: [],
-            type: 'function',
-            stateMutability: 'nonpayable',
-            payable: false,
-          },
-        ] as const,
-        this.TOKEN_ADDRESS,
-      );
+      // const contract = new this.web3.eth.Contract(
+      //   [
+      //     {
+      //       constant: false,
+      //       inputs: [
+      //         { name: 'account', type: 'address' },
+      //         { name: 'amount', type: 'uint256' }
+      //       ],
+      //       name: 'transferFromFiat',
+      //       outputs: [],
+      //       type: 'function',
+      //       stateMutability: 'nonpayable',
+      //       payable: false
+      //     }
+      //   ] as const,
+      //   this.TOKEN_ADDRESS
+      // );
 
+      // const amountWithFee = BigInt(Math.floor(amount * 10 ** 18 * (1 - this.PLATFORM_FEE)));
+      // const data = contract.methods.transferFromFiat(address, amountWithFee).encodeABI();
+
+      // const tx = {
+      //   from: this.ADMIN_ADDRESS,
+      //   to: this.TOKEN_ADDRESS,
+      //   gas: '1000000',
+      //   data: data
+      // };
+
+      // const signedTx = await this.web3.eth.accounts.signTransaction(tx, this.ADMIN_PRIVATE_KEY);
+      // const receipt = await this.web3.eth.sendSignedTransaction(signedTx.rawTransaction);
+
+      // return receipt.status === BigInt(1);
+      const contract = mockContractResponse;
       const amountWithFee = BigInt(
         Math.floor(amount * 10 ** 18 * (1 - this.PLATFORM_FEE)),
       );
-      const data = contract.methods
-        .transferFromFiat(address, amountWithFee)
-        .encodeABI();
-
-      const tx = {
-        from: this.ADMIN_ADDRESS,
-        to: this.TOKEN_ADDRESS,
-        gas: '1000000',
-        data: data,
-      };
-
-      const signedTx = await this.web3.eth.accounts.signTransaction(
-        tx,
-        this.ADMIN_PRIVATE_KEY,
-      );
-      const receipt = await this.web3.eth.sendSignedTransaction(
-        signedTx.rawTransaction,
-      );
-
-      return receipt.status === BigInt(1);
+      return true;
     } catch (error) {
       console.error('Error in transferFromFiat:', error);
       throw error;
@@ -118,45 +124,41 @@ export class EthereumService {
     userPrivateKey: string,
   ): Promise<boolean> {
     try {
-      const contract = new this.web3.eth.Contract(
-        [
-          {
-            constant: false,
-            inputs: [
-              { name: 'account', type: 'address' },
-              { name: 'amount', type: 'uint256' },
-            ],
-            name: 'transferToFiat',
-            outputs: [],
-            type: 'function',
-            stateMutability: 'nonpayable',
-            payable: false,
-          },
-        ] as const,
-        this.TOKEN_ADDRESS,
-      );
+      // const contract = new this.web3.eth.Contract(
+      //   [
+      //     {
+      //       constant: false,
+      //       inputs: [
+      //         { name: 'account', type: 'address' },
+      //         { name: 'amount', type: 'uint256' },
+      //       ],
+      //       name: 'transferToFiat',
+      //       outputs: [],
+      //       type: 'function',
+      //       stateMutability: 'nonpayable',
+      //       payable: false,
+      //     },
+      //   ] as const,
+      //   this.TOKEN_ADDRESS,
+      // );
 
+      // const amountWithFee = BigInt(Math.floor(amount * 10 ** 18));
+      // const data = contract.methods.transferToFiat(address, amountWithFee).encodeABI();
+
+      // const tx = {
+      //   from: this.ADMIN_ADDRESS,
+      //   to: this.TOKEN_ADDRESS,
+      //   gas: '1000000',
+      //   data: data
+      // };
+
+      // const signedTx = await this.web3.eth.accounts.signTransaction(tx, userPrivateKey);
+      // const receipt = await this.web3.eth.sendSignedTransaction(signedTx.rawTransaction);
+
+      // return receipt.status === BigInt(1);
+      const contract = mockContractResponse;
       const amountWithFee = BigInt(Math.floor(amount * 10 ** 18));
-      const data = contract.methods
-        .transferToFiat(address, amountWithFee)
-        .encodeABI();
-
-      const tx = {
-        from: this.ADMIN_ADDRESS,
-        to: this.TOKEN_ADDRESS,
-        gas: '1000000',
-        data: data,
-      };
-
-      const signedTx = await this.web3.eth.accounts.signTransaction(
-        tx,
-        userPrivateKey,
-      );
-      const receipt = await this.web3.eth.sendSignedTransaction(
-        signedTx.rawTransaction,
-      );
-
-      return receipt.status === BigInt(1);
+      return true;
     } catch (error) {
       console.error('Error in transferToFiat:', error);
       throw error;
@@ -169,45 +171,41 @@ export class EthereumService {
     userPrivateKey: string,
   ): Promise<boolean> {
     try {
-      const contract = new this.web3.eth.Contract(
-        [
-          {
-            constant: false,
-            inputs: [
-              { name: 'to', type: 'address' },
-              { name: 'amount', type: 'uint256' },
-            ],
-            name: 'transfer',
-            outputs: [],
-            type: 'function',
-            stateMutability: 'nonpayable',
-            payable: false,
-          },
-        ] as const,
-        this.TOKEN_ADDRESS,
-      );
+      // const contract = new this.web3.eth.Contract(
+      //   [
+      //     {
+      //       constant: false,
+      //       inputs: [
+      //         { name: 'to', type: 'address' },
+      //         { name: 'amount', type: 'uint256' },
+      //       ],
+      //       name: 'transfer',
+      //       outputs: [],
+      //       type: 'function',
+      //       stateMutability: 'nonpayable',
+      //       payable: false,
+      //     },
+      //   ] as const,
+      //   this.TOKEN_ADDRESS,
+      // );
 
+      // const amountWithFee = BigInt(Math.floor(amount * 10 ** 18));
+      // const data = contract.methods.transfer(address, amountWithFee).encodeABI();
+
+      // const tx = {
+      //   from: this.ADMIN_ADDRESS,
+      //   to: this.TOKEN_ADDRESS,
+      //   gas: '1000000',
+      //   data: data
+      // };
+
+      // const signedTx = await this.web3.eth.accounts.signTransaction(tx, userPrivateKey);
+      // const receipt = await this.web3.eth.sendSignedTransaction(signedTx.rawTransaction);
+
+      // return receipt.status === BigInt(1);
+      const contract = mockContractResponse;
       const amountWithFee = BigInt(Math.floor(amount * 10 ** 18));
-      const data = contract.methods
-        .transfer(address, amountWithFee)
-        .encodeABI();
-
-      const tx = {
-        from: this.ADMIN_ADDRESS,
-        to: this.TOKEN_ADDRESS,
-        gas: '1000000',
-        data: data,
-      };
-
-      const signedTx = await this.web3.eth.accounts.signTransaction(
-        tx,
-        userPrivateKey,
-      );
-      const receipt = await this.web3.eth.sendSignedTransaction(
-        signedTx.rawTransaction,
-      );
-
-      return receipt.status === BigInt(1);
+      return true;
     } catch (error) {
       console.error('Error in transfer:', error);
       throw error;
--- src/config/ethereum/mocks/ethereum.mocks.ts
+++ src/config/ethereum/mocks/ethereum.mocks.ts
@@ -3,7 +3,24 @@ export const mockTokenBalance = '1000000000000000000'; // 1 ESOM в wei
 export const mockContractResponse = {
   methods: {
     balanceOf: (address: string) => ({
-      call: async () => mockTokenBalance,
+      call: async () => '1000000000000000000', // 1 токен в wei
+    }),
+    transferFromFiat: (address: string, amount: bigint) => ({
+      call: async () => true,
+    }),
+    transferToFiat: (address: string, amount: bigint) => ({
+      call: async () => true,
+    }),
+    transfer: (address: string, amount: bigint) => ({
+      call: async () => true,
     }),
   },
+};
+
+export const mockTransactionReceipt = {
+  status: BigInt(1),
+  transactionHash: '0x123...',
+  blockHash: '0x456...',
+  blockNumber: 12345,
+  gasUsed: '1000000',
 };
