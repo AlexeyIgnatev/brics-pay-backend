85ecaf0d45b144c4085aa81bc4907b7264b52f01
BogdanKur <bananikyt0@gmail.com>
2026-06-23 09:18:48 +0300

    Fix SHKeeper Prisma injection

--- src/config/exchange/exchange.module.ts
+++ src/config/exchange/exchange.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
+import { PrismaClient } from '@prisma/client';
 import { ShkeeperExchangeService } from './shkeeper.service';
 import { ShkeeperWalletService } from './shkeeper-wallet.service';
 
 @Module({
-  providers: [ShkeeperExchangeService, ShkeeperWalletService],
+  providers: [PrismaClient, ShkeeperExchangeService, ShkeeperWalletService],
   exports: [ShkeeperExchangeService, ShkeeperWalletService],
 })
 export class ExchangeModule {}
