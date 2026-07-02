38bc6df3f82dea3296a49e4f27d0fbae567b7a53
BogdanKur <bananikyt0@gmail.com>
2026-07-02 16:00:43 +0300

    Rebuild local USDT with legacy Solidity

--- contracts/LocalUsdt.sol
+++ contracts/LocalUsdt.sol
@@ -1,13 +1,12 @@
 // SPDX-License-Identifier: UNLICENSED
-pragma solidity 0.8.17;
+pragma solidity 0.5.17;
 
 contract LocalUsdt {
-    string public constant name = "USDT";
-    string public constant symbol = "USDT";
-    uint8 public constant decimals = 6;
-
-    address public owner;
+    string public name = "USDT";
+    string public symbol = "USDT";
+    uint8 public decimals = 6;
     uint256 public totalSupply;
+    address public owner;
 
     mapping(address => uint256) public balanceOf;
     mapping(address => mapping(address => uint256)) public allowance;
@@ -20,23 +19,23 @@ contract LocalUsdt {
         _;
     }
 
-    constructor(address initialHolder, uint256 initialSupply) {
+    constructor(address initialHolder, uint256 initialSupply) public {
         owner = msg.sender;
         _mint(initialHolder, initialSupply);
     }
 
-    function approve(address spender, uint256 amount) external returns (bool) {
+    function approve(address spender, uint256 amount) public returns (bool) {
         allowance[msg.sender][spender] = amount;
         emit Approval(msg.sender, spender, amount);
         return true;
     }
 
-    function transfer(address to, uint256 amount) external returns (bool) {
+    function transfer(address to, uint256 amount) public returns (bool) {
         _transfer(msg.sender, to, amount);
         return true;
     }
 
-    function transferFrom(address from, address to, uint256 amount) external returns (bool) {
+    function transferFrom(address from, address to, uint256 amount) public returns (bool) {
         uint256 allowed = allowance[from][msg.sender];
         require(allowed >= amount, "insufficient allowance");
         allowance[from][msg.sender] = allowed - amount;
@@ -44,22 +43,19 @@ contract LocalUsdt {
         return true;
     }
 
-    function mint(address to, uint256 amount) external onlyOwner returns (bool) {
+    function mint(address to, uint256 amount) public onlyOwner returns (bool) {
         _mint(to, amount);
         return true;
     }
 
-    function renounceOwnership() external onlyOwner {
+    function renounceOwnership() public onlyOwner {
         owner = address(0);
     }
 
     function _transfer(address from, address to, uint256 amount) internal {
         require(to != address(0), "zero address");
-        uint256 fromBalance = balanceOf[from];
-        require(fromBalance >= amount, "insufficient balance");
-        unchecked {
-            balanceOf[from] = fromBalance - amount;
-        }
+        require(balanceOf[from] >= amount, "insufficient balance");
+        balanceOf[from] -= amount;
         balanceOf[to] += amount;
         emit Transfer(from, to, amount);
     }
