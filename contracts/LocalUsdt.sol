1f1090714c442d74460d185d4ad3d924f487cb7a
BogdanKur <bananikyt0@gmail.com>
2026-07-02 15:48:59 +0300

    Recompile local USDT for TRON compatibility

---
+++ contracts/LocalUsdt.sol
@@ -0,0 +1,73 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity 0.8.24;
+
+contract LocalUsdt {
+    string public constant name = "USDT";
+    string public constant symbol = "USDT";
+    uint8 public constant decimals = 6;
+
+    address public owner;
+    uint256 public totalSupply;
+
+    mapping(address => uint256) public balanceOf;
+    mapping(address => mapping(address => uint256)) public allowance;
+
+    event Transfer(address indexed from, address indexed to, uint256 value);
+    event Approval(address indexed owner, address indexed spender, uint256 value);
+
+    modifier onlyOwner() {
+        require(msg.sender == owner, "only owner");
+        _;
+    }
+
+    constructor(address initialHolder, uint256 initialSupply) {
+        owner = msg.sender;
+        _mint(initialHolder, initialSupply);
+    }
+
+    function approve(address spender, uint256 amount) external returns (bool) {
+        allowance[msg.sender][spender] = amount;
+        emit Approval(msg.sender, spender, amount);
+        return true;
+    }
+
+    function transfer(address to, uint256 amount) external returns (bool) {
+        _transfer(msg.sender, to, amount);
+        return true;
+    }
+
+    function transferFrom(address from, address to, uint256 amount) external returns (bool) {
+        uint256 allowed = allowance[from][msg.sender];
+        require(allowed >= amount, "insufficient allowance");
+        allowance[from][msg.sender] = allowed - amount;
+        _transfer(from, to, amount);
+        return true;
+    }
+
+    function mint(address to, uint256 amount) external onlyOwner returns (bool) {
+        _mint(to, amount);
+        return true;
+    }
+
+    function renounceOwnership() external onlyOwner {
+        owner = address(0);
+    }
+
+    function _transfer(address from, address to, uint256 amount) internal {
+        require(to != address(0), "zero address");
+        uint256 fromBalance = balanceOf[from];
+        require(fromBalance >= amount, "insufficient balance");
+        unchecked {
+            balanceOf[from] = fromBalance - amount;
+        }
+        balanceOf[to] += amount;
+        emit Transfer(from, to, amount);
+    }
+
+    function _mint(address to, uint256 amount) internal {
+        require(to != address(0), "zero address");
+        totalSupply += amount;
+        balanceOf[to] += amount;
+        emit Transfer(address(0), to, amount);
+    }
+}
