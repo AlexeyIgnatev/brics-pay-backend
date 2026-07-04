3891e280720dd68c61aca83dd0d701c9dcfd415f
BogdanKur <bananikyt0@gmail.com>
2026-07-04 12:24:02 +0300

    Allow partial settings updates

--- src/blockchain-config/dto/settings-partial.dto.ts
+++ src/blockchain-config/dto/settings-partial.dto.ts
@@ -1,14 +1,26 @@
-import { IsString } from 'class-validator';
+import { IsOptional, IsString } from 'class-validator';
 
 export class SettingsPartialDto {
-  @IsString() esom_per_usd?: string;
+  @IsOptional()
+  @IsString()
+  esom_per_usd?: string;
 
-  @IsString() esom_som_conversion_fee_pct?: string;
-  @IsString() esom_som_conversion_fee_min?: string;
+  @IsOptional()
+  @IsString()
+  esom_som_conversion_fee_pct?: string;
+  @IsOptional()
+  @IsString()
+  esom_som_conversion_fee_min?: string;
 
-  @IsString() usdt_trade_fee_pct?: string;
+  @IsOptional()
+  @IsString()
+  usdt_trade_fee_pct?: string;
 
-  @IsString() usdt_withdraw_fee_fixed?: string;
+  @IsOptional()
+  @IsString()
+  usdt_withdraw_fee_fixed?: string;
 
-  @IsString() min_withdraw_usdt_trc20?: string;
+  @IsOptional()
+  @IsString()
+  min_withdraw_usdt_trc20?: string;
 }
